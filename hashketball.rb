9b5bf47bea73968991e3354fd577e4b46afcb765
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-03-02 05:53:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,7 +190,7 @@ def big_shoe_rebounds
   game_hash.each do |team, team_info|
    team_info[:players].each do |player|
      binding.pry
-      player[:shoe].each do |size|
+      if player[:shoe] > max_shoe
     end
   end
 end
